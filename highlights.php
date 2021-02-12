3459008bf18fa21ab093c5797637f38da8ca5a0e
LyndonTomas <201801180@iacademy.edu.ph>
2021-02-12 23:44:41 +0800

    seventh commit

--- highlights.php
+++ highlights.php
@@ -1,1 +1,52 @@
-<?php include_once("pages/portfolio.html")?>
+<?php <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lyndon's Site - Highlights</title>
+    <!-- Bootstrap Necessities -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <!-- End of Bootstrap Necessities -->
+    <!-- icon -->
+    <link rel="icon" href="images/logo.png">
+    <!-- css -->
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+        <div class="container-fluid">
+            <a href="index.php" class="navbar-brand">
+                <!-- image logo -->
+                <img class="logo" src="images/logo.png" alt="logo" title="Home">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="about.php" class="nav-link">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="portfolio.php" class="nav-link">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="highlights.php" class="nav-link">Highlights</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="contact.php" class="nav-link">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+
+?>
